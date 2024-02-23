c6face8c515a39b287c848b0f691257d03d62664
Luca Utzeri <lutzeri@libero.it>
2024-02-23 10:28:08 +0100

    Initial commit

---
+++ src/main/java/lucautzeri/ProgettoBEU2S3/ProgettoBeu2S3Application.java
@@ -0,0 +1,13 @@
+package lucautzeri.ProgettoBEU2S3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBeu2S3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoBeu2S3Application.class, args);
+	}
+
+}
---
+++ src/test/java/lucautzeri/ProgettoBEU2S3/ProgettoBeu2S3ApplicationTests.java
@@ -0,0 +1,13 @@
+package lucautzeri.ProgettoBEU2S3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoBeu2S3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
